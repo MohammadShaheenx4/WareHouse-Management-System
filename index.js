6c631fd6a17a3bf5f254bbfe98ef485aa273a969
Mohammad Shaheen <hamode.sh889@gmail.com>
2025-04-17 16:25:05 +0300

    dotenv

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import initApp from './src/app.router.js';
-import 'dotenv/config'
+require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
