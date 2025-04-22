1b9891e51eb6a71fa3b0f21f81f8a13a8c9b1530
Mohammad Shaheen <hamode.sh889@gmail.com>
2025-04-22 14:38:09 +0300

    order from supplier

--- src/app.router.js
+++ src/app.router.js
@@ -15,6 +15,8 @@ const initApp = async (app, express) => {
     app.get('/', (req, res) => {
         return res.status(200).json({ message: "welcome......" });
     });
+    app.use(express.json({ limit: '50mb' }));
+    app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
     app.use("/auth", authRouter);
     app.use("/category", categoryRouter);
